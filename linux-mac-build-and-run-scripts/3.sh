0b1fe41da45869d90480b8929f376507fd2b12b9
JohnLevander <john.d.levander@gmail.com>
2021-08-27 11:37:02 -0700

    updated after texting on linux

--- linux-mac-build-and-run-scripts/3.sh
+++ linux-mac-build-and-run-scripts/3.sh
@@ -1,3 +1,11 @@
-(cd ../dphe-cli/target/deepphe-0.4.0-bin/deepphe-0.4.0 && java -Xmx3g -Xms512M -classpath ./resources/:./lib/* org.apache.ctakes.core.pipeline.PiperFileRunner -p pipeline/DeepPhe.piper -i ../../../dphe-examples/reports -o ../../../output/DeepPhe -r bolt://localhost:7687 --user neo4j --pass neo4jpass)
+#!/bin/bash
+
+git clone --depth 1 --branch v20210827 https://github.com/DeepPhe/dphe-examples.git
+
+_dir=$('pwd')
+
+mkdir -p output
+
+(cd ../dphe-cli/target/deepphe-0.4.0-bin/deepphe-0.4.0 && java -Xmx3g -Xms512M -classpath ./resources/:./lib/* org.apache.ctakes.core.pipeline.PiperFileRunner -p pipeline/DeepPhe.piper -i $_dir/dphe-examples/reports/ -o $_dir/output -r bolt://localhost:7687 --user neo4j --pass neo4jpass)
 
 
