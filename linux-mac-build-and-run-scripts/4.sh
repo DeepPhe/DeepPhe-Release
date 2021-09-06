89c376be9b5fa6057887e6b91b6c4baf06812a88
John Levander <john.d.levander@gmail.com>
2021-09-06 19:08:02 -0400

    rearranged the order the poms build, added more user friendly code for automatically installing npm, updated git ignore to ignore .DS_Store files

--- linux-mac-build-and-run-scripts/4.sh
+++ linux-mac-build-and-run-scripts/4.sh
@@ -1,3 +1,23 @@
+
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    which -s brew
+	if [[ $? != 0 ]] ; then
+	    # Install Homebrew
+	    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	else
+	    brew update
+	fi     
+	npm install node
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    nvm install -g npm
+fi
+
+
+
+
 git clone --depth 1 --branch v20210826 https://github.com/DeepPhe/DeepPhe-Viz.git
 
 #start the api/webserver
