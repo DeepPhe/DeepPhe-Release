7ddd5aa5d8f0287f09c8bc45e56be53dd2978605
Sean Finan <sean.finan@childrens.harvard.edu>
2022-01-25 15:28:22 -0500

    added missing Imports

--- dphe-stream/src/main/java/org/healthnlp/deepphe/nlp/ae/PipelineStopper.java
+++ dphe-stream/src/main/java/org/healthnlp/deepphe/nlp/ae/PipelineStopper.java
@@ -1,8 +1,12 @@
 package org.healthnlp.deepphe.nlp.ae;
 
+import org.apache.log4j.Logger;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.jcas.JCas;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author SPF , chip-nlp
